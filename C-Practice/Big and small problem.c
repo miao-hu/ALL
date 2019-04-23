74ca39be33f0cc1db2abcf75257015d13e27879e
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-04-23 19:43:14 +0800

    Update Big and small problem.c
    设计一个小程序来判断当前机器的字节序

--- C-Practice/Big and small problem.c
+++ C-Practice/Big and small problem.c
@@ -14,3 +14,40 @@ int main()
 }
 
 //计算机：小端模式
+
+
+//设计一个小程序来判断当前机器的字节序
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int check_sys()
+//{
+//	int i = 1;  //0000 0000 0000 0000 0000 0000 0000 0001
+//	return(*((char *)&i));
+//}
+
+int check_sys()
+{
+	union
+	{
+		int i;
+		char c;
+	}un;
+	un.i = 1;
+	return un.c;
+}
+int main()
+{
+	int ret = check_sys();
+	if (1 == ret)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+	system("pause");
+	return 0;
+}
