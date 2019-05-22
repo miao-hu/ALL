f54a1c7a1a038bf55fbabf2a23fcf1ed704f16c1
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-05-22 13:04:23 +0800

    Delete TestComparableCircle.java

--- JAVA-Practice/ComparableCircle/TestComparableCircle.java
+++
@@ -1,8 +0,0 @@
-public class  TestComparableCircle{
-
-    public static void main(String[] args){
-        ComparableCircle c1 = new ComparableCircle(2.0);
-        ComparableCircle c2 = new ComparableCircle(3.0);
-        System.out.println(c1.compareTo(c2));
-    }
-}
