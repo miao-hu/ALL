7b5b1f5e32353454c0171c1b65201d564646ee13
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-05-22 13:02:34 +0800

    Create Circle.java

---
+++ JAVA-Practice/The ComparableCircle class/Circle.java
@@ -0,0 +1,29 @@
+public class Circle {
+
+    private double radius;
+
+   
+
+    public Circle(){
+
+        radius = 1.0;
+
+    }
+
+
+
+    public Circle(double radius){
+
+        this.radius = radius;
+
+    }
+
+
+
+    public double getArea(){
+
+        return Math.PI * radius * radius;
+
+    }
+
+}
