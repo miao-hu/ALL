437920a2738e434a1e6f6310b704308b4585a888
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-05-27 12:51:59 +0800

    Create GeometricObiect.java

---
+++ JAVA-Practice/The Colorable interface/GeometricObiect.java
@@ -0,0 +1,48 @@
+public abstract class GeometricObject {  //定义一个抽象类GeometricObject
+    private String color = "white";
+    private boolean filled;
+    private java.util.Date dateCreated;   //java.util.Date是类名
+
+	protected GeometricObject(){
+        dateCreated = new java.util.Date();
+    }
+	
+    protected  GeometricObject (String color,boolean filled){
+       dateCreated = new java.util.Date();
+	   this.color=color;
+	   this.filled=filled;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public boolean isFilled(){
+        return filled;
+    }
+
+    public void setFilled(boolean filled){
+        this.filled = filled;
+    }
+
+    public java.util.Date getDateCreated(){
+        return dateCreated;
+    }
+
+    @Override
+    public String toString(){
+        return "created on " + dateCreated +
+				"\ncolor: " + color +
+				"\nfilled: " + filled;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPrimeter();
+
+}
+
