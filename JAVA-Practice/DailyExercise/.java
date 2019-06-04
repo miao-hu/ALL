56dfaf17996d59711bf6121ed9e2c5986734c604
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-06-04 12:02:32 +0800

    Update .java

--- JAVA-Practice/DailyExercise/.java
+++ JAVA-Practice/DailyExercise/.java
@@ -1,6 +1,5 @@
-1.找到能被5或被6整除，但不能同时被5或6整除的数。编写一个程序，显示从100到200中，
+1.（找到能被5或被6整除，但不能同时被5或6整除的数）编写一个程序，显示从100到200中，
 每十个数一行，能被5或被6整除，但不能同时被5或6整除的数，数字之间用一个空格隔开。
-
 
 import java.util.Scanner; 
 public class DivBy5or6ButNotBoth{
@@ -14,5 +13,30 @@ public class DivBy5or6ButNotBoth{
 				System.out.println();			
 			}		
 		}	
+	}
+}
+
+
+
+2.（求一个整数的各位数字之和）编写一个方法，计算一个整数各位数字之和。
+   使用方法头public	static	int	sumDigits(long n)
+   例如：sumDigits(234)返回9（2+3+4）
+   
+import java.util.Scanner;
+public class SumDigitsInAnInteger{	
+	public static void main(String[] args){	
+	System.out.print("plese enter an int number:");
+	Scanner input = new Scanner(System.in);		
+	int num = input.nextInt();	
+	System.out.println("The sum is "+sumDigits(num));	
+	}	
+	
+	public static int sumDigits(int n){	
+		int sum = 0;		
+		while(n != 0){	
+			sum += n % 10;		
+			n = n / 10;		
+		}		
+	return sum;	
 	}
 }
