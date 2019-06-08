aebf84b78d9d8b614ebcbf447fdab12973df278b
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-06-08 21:46:43 +0800

    Update .java

--- JAVA-Practice/DailyExercise/.java
+++ JAVA-Practice/DailyExercise/.java
@@ -134,3 +134,36 @@ public class AverageArray{
  } 
 
 
+
+6.（按考分对学生排序）以正确答案个数升序显示学生。
+
+	public class SortStudentOnGrades{	
+	public static void main(String[] args){	
+		char answer[][] = {		
+		{'A','B','A','C','C','D','E','E','A','D'},		
+		{'D','B','A','B','C','A','E','E','A','D'},	
+		{'E','D','D','A','C','B','E','E','A','D'},		
+		{'C','B','A','E','D','C','E','E','A','D'},		
+		{'A','B','D','C','C','D','E','E','A','D'},		
+		{'B','B','E','C','C','D','E','E','A','D'},		
+		{'B','B','A','C','C','D','E','E','A','D'},		
+		{'E','B','E','C','C','D','E','E','A','D'}};				
+	char keys[] = {'D','B','D','C','C','D','A','E','A','D'};	
+	int[] student = new int[answer.length];	
+	for(int i = 0; i < answer.length; i++){	
+		int correctCount = 0;		
+		for(int j = 0; j < answer[i].length; j++){
+			if(answer[i][j] == keys[j])			
+			correctCount++;	
+		}		
+		student[i] = correctCount;	
+	}	
+	java.util.Arrays.sort(student);			
+	for(int i = 0; i < student.length; ++i){
+			System.out.println(student[i]);
+	}
+	}
+}
+
+
+
