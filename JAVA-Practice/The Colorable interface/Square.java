70795e4b033a91035d3bf28ac7a6ec64ada1d82f
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-06-19 12:57:07 +0800

    Update Square.java

--- JAVA-Practice/The Colorable interface/Square.java
+++ JAVA-Practice/The Colorable interface/Square.java
@@ -1,3 +1,13 @@
+//(可着色接口Colorable)设计一个名为Colorable的接口，其中有名为()的void方法，可着色对象的每个类必须实现这个接口，
+//		      设计一个扩展GeometricObject的类并实现Colorable接口的名为Square的类，
+//		      实现howToColor方法在这个类中，显示消息”Color all four sides”。
+
+
+//			编写一个测试程序，创建有5个GeometricObject对象的数组，对于数组中的每个对象而言，
+//			如果对象是可着色的（已被填充的），那就展示他的面积个调用howToColor()方法。
+
+
+
 public class Square extends GeometricObject implements Colorable{
 	//一个名为Square的类：扩展GeometricObject类并且实现Colorable接口
     private double side;
