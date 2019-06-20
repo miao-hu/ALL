26a90ca5c43b3c9fd9799b30837efbaaaef4c63f
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-06-20 12:33:02 +0800

    Update Account.java

--- JAVA-Practice/The Account class/Account.java
+++ JAVA-Practice/The Account class/Account.java
@@ -1,3 +1,21 @@
+//（账户类Account）设计一个名为Account的类，他包括：
+//		  *一个名为id的Int 类型的私有账户数据域（默认值为0）
+//		  *一个名为balance的double类型的私有账户数据域（默认值为0）
+//		  *一个名为annualInterestRate的double类型的私有数据域存储当前的利率（默认值为0），假设所有的账户都有相同的利率
+//		  *一个名为dataCreated的Date型的私有数据与存储账户的开户日期
+//		  *一个能创建默认账户的的无参构造方法
+//		  *一个能创建带指定的id和初始余额账户的构造方法
+//		  *Id,balance,annualInterestRare的访问器和修改器
+//		  *dataCreated的访问器
+//		  *一个名为getMonthlyInterestRate()方法返回月利率
+//		  *一个名为withdraw()方法从账户提取特定数额
+//	  	  *一个名为deposit()方法向账户存储特定的数额
+
+//		实现这个类，编写一个测试程序，创建一个账户id为1122，余额为200000美元，年利率为4.5%的Account对象。
+//		使用withdraw()方法提款2500美元，使用desposit()方法存款3000美元，打印余额，月利率以及这个账户的开户日期。
+
+
+
 import java.util.Date;           //这个类提供了与系统无关的对日期和时间的封装
 
 public class Account {           //一个名为Account的类
