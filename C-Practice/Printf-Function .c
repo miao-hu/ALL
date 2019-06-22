66655a3104da7374f38fd72e48511173594c9c5f
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-06-22 20:34:53 +0800

    Update Printf-Function .c

--- C-Practice/Printf-Function .c
+++ C-Practice/Printf-Function .c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+
+1.
 int main()
 {
 	printf("%d", printf("%d", printf("%d", 43)));
@@ -9,3 +11,14 @@ int main()
 }
 //结果为4321,printf实际控制输出的字符数，返回值为字符个数
 //先输出43，然后43是两个字符，返回2 ，因此输出2，然后2是一个字符，返回1，因此结果为4321
+
+
+
+2.
+int main()
+{
+	printf("hello""bit\n");   //hellobit
+	printf("hello"",""bit");  //hello,bit
+	system("pause");
+	return 0;           //相邻字符串进行连接
+}
