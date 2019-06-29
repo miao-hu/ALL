686288e226ffc29e42390741e851b4428aa0a133
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-06-29 13:04:08 +0800

    Update .asm

--- Assembly-Practice/.asm
+++ Assembly-Practice/.asm
@@ -61,6 +61,51 @@ print:  mov ah,2
         int 21h
 code    ends
         end begin
+      
+      
         
+3. 要求在数据段定义一个dw类型的数据，编写程序将其以十进制形式在屏幕上显示。 
+
+data    segment
+dat1    dw     0ffffh
+data    ends
+
+code    segment
+        assume cs:code,ds:data
+begin:
+        mov ax,data
+        mov ds,ax
+
+        mov bx,dat1
+        mov cx,10000
+        call dec_div
+        mov cx,1000
+        call dec_div
+        mov cx,100
+        call dec_div
+        mov cx,10
+        call dec_div
+        mov cx,1
+        call dec_div
         
-        
+        mov ah,4ch
+        int 21h
+
+dec_div proc
+        mov dx,0
+        mov ax,bx
+        div cx
+        mov bx,dx
+        mov dl,al
+        add dl,30h
+        mov ah,2
+        int 21h
+        ret
+dec_div endp
+code    ends
+        end begin
+
+
+
+
+
