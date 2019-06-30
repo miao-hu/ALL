94e57bc26072ad5d544aa609c379f3c741821d23
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-06-30 17:45:53 +0800

    Update .asm

--- Assembly-Practice/.asm
+++ Assembly-Practice/.asm
@@ -107,5 +107,90 @@ code    ends
 
 
 
+4.将上述3个程序功能改写成子程序，在主程序中调用，实现将定义的dw类型数据，分别以二进制、十六进制、十进制形式在屏幕上显示输出。
+
+data    segment
+dat1    dw     0ffffh
+data    ends
+
+code    segment
+        assume cs:code,ds:data
+begin:
+        mov ax,data
+        mov ds,ax
+
+		call bin_show
+		call hex_show
+		call dec_show
+
+		mov ah,4ch
+        int 21h
+
+hex_show proc
+        mov bx,dat1
+        mov cx,4
+
+t2:     push cx
+        mov cl,4
+        rol bx,cl
+        pop cx
+        mov dl,bl
+        and dl,0fh
+        add dl,30h
+        cmp dl,39h
+        jbe print
+        add dl,7
+print:  mov ah,2
+        int 21h
+        loop t2
+		ret
+hex_show endp
+
+bin_show proc
+        mov bx,dat1
+        mov cx,16
+
+t1:     rol bx,1
+        mov dl,bl
+        and dl,01h
+        add dl,30h
+        mov ah,2
+        int 21h
+        loop t1
+		ret
+bin_show endp
+
+dec_show proc
+        mov bx,dat1
+        mov cx,10000
+        call dec_div
+        mov cx,1000
+        call dec_div
+        mov cx,100
+        call dec_div
+        mov cx,10
+        call dec_div
+        mov cx,1
+        call dec_div
+		ret
+dec_show endp
+        
+    
+
+dec_div proc
+        mov dx,0
+        mov ax,bx
+        div cx
+        mov bx,dx
+        mov dl,al
+        add dl,30h
+        mov ah,2
+        int 21h
+        ret
+dec_div endp
+code    ends
+        end begin
+
+
 
 
