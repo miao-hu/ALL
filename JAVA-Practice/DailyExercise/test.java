3919a489d3218f2b6aee76759b9a2c42d7d5e4cb
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-07-22 14:23:41 +0800

    Update test.java

--- JAVA-Practice/DailyExercise/test.java
+++ JAVA-Practice/DailyExercise/test.java
@@ -423,4 +423,54 @@ public class DecToHex{
 
 
 
-14.
+14./**
+     * 10 进制转 16 进制，计算公式如下例
+     * 1958 转 16 进制
+     * 1958 % 16 == 6   1958 / 16 == 122
+     * 122 % 16 == 10 也就是 A   122 / 16 == 7
+     * 7 < 16
+     * 1958 的 16 进制 为 7A6
+     * @param n
+     * @return
+     */
+
+import java.util.Scanner;
+
+public class DecToHex{
+	public static void main(String[] args){
+		System.out.print("please enter a number:");
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		String s="";
+		while(n>0){
+			int yushu=n%16;
+			if(yushu<10){
+				s+=yushu;
+			}
+			else{
+				char c=(char)((yushu-10)+'A');
+				s+=c;
+			}
+			n/=16;
+		}
+		System.out.println(reverse(s));
+	}
+    public static String reverse(String s){
+        int length=s.length();
+        String r ="";
+        for (int i=length-1;i>=0;i--) {
+            r+=s.charAt(i);
+        }
+        return r;
+    }
+}
+
+
+
+15.
+	
+	
+	
+	
+	
+	
