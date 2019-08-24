d395b615f5047a94dc97e907f2f7b6ad649b973c
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-08-24 09:16:23 +0800

    Update test.java

--- JAVA-Practice/DailyExercise/test.java
+++ JAVA-Practice/DailyExercise/test.java
@@ -2430,7 +2430,140 @@ public class Solution {
 
 
 
-39.
+39.class Node{
+    int val;
+    Node next=null;
+    Node(int val){
+        this.val=val;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Node head=createList();
+        Node result=new Solution().removeAll(head, 6);
+        for(Node cur=head;cur!=null;cur=cur.next){
+            System.out.print(cur.val+"-->");
+        }
+        System.out.print("null");//6-->1-->2-->3-->null
+        System.out.println();
+        System.out.println("=====================");
+        for(Node cur=result;cur!=null;cur=cur.next){
+            System.out.print(cur.val+"-->");
+        }
+        System.out.print("null");//1-->2-->3-->null
+        System.out.println();
+        System.out.println("=====================");
+        Node head1=createList1();
+        Node head2=createList2();
+        Node merged=new Solution().merge(head1, head2);
+        for(Node cur=merged;cur!=null;cur=cur.next){
+            System.out.print(cur.val+"-->");
+        }
+        System.out.print("null");//1-->2-->3-->6-->null
+    }
+
+    public static Node createList(){
+        Node p1=new Node(6);
+        Node p2=new Node(1);
+        Node p3=new Node(6);
+        Node p4=new Node(6);
+        Node p5=new Node(2);
+        Node p6=new Node(3);
+        p1.next=p2;
+        p2.next=p3;
+        p3.next=p4;
+        p4.next=p5;
+        p5.next=p6;
+        return p1;
+    }
+
+    public static Node createList1(){
+        Node p1=new Node(1);
+        Node p2=new Node(6);
+        p1.next=p2;
+        return p1;
+    }
+
+    public static Node createList2(){
+        Node p1=new Node(2);
+        Node p2=new Node(3);
+        p1.next=p2;
+        return p1;
+    }
+
+    Node removeAll(Node head, int val){
+        Node prev=null;
+        Node cur=head;
+        while(cur!=null){
+            if(cur.val==val){
+                if(cur==head){
+                    head=cur.next;
+                }
+                else{
+                    prev.next=cur.next;
+                }
+            }
+            else{
+                prev=cur;
+            }
+            cur=cur.next;
+        }
+        return head;
+    }
+
+    Node merge(Node head1, Node head2){
+        if(head1==null)return head2;
+        if(head2==null)return head1;
+        Node result=null;
+        Node last=null;
+        Node cur1=head1;
+        Node cur2=head2;
+        while(cur1!=null&&cur2!=null){
+            if(cur1.val<cur2.val){
+                if(result==null){
+                    result=cur1;
+                }
+                else{
+                    last.next=cur1;
+                }
+                last=cur1;
+                cur1=cur1.next;
+            }
+            else{
+                if(result==null){
+                    result=cur2;
+                }
+                else{
+                    last.next=cur2;
+                }
+                last=cur2;
+                cur2=cur2.next;
+            }
+        }
+        if(cur1!=null){
+            last.next=cur1;
+        }
+        else{
+            last.next=cur2;
+        }
+        return result;
+    }
+}
+
+
+
+40.
+
+
+
+
+
+
+
+
+
+
 
 
 
