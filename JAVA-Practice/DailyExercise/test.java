073b2e83b580b719bcf4444be883e670f80c6005
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-08-26 19:07:13 +0800

    Update test.java

--- JAVA-Practice/DailyExercise/test.java
+++ JAVA-Practice/DailyExercise/test.java
@@ -2553,7 +2553,116 @@ public class Solution {
 
 
 
-40.
+40./*这是一份错误的代码
+  通过这份代码学会简单的调试
+*/
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode(int val) {
+        this(val, null); //调用另一个构造方法
+    }
+}
+
+
+public class LinkedListInterview {
+    public static void main(String[] args) {
+        test();
+    }
+
+    // 三个重要节点
+    // 1. 咱创建的测试链表是不是出问题了？
+    // 2. 咱的分离程序是不是出问题了？
+    // 3. 咱的打印程序是不是出问题了？
+
+    private static void test() {
+        // 4 5 2 7 6 3 8 1
+        ListNode head = createTestList();
+        ListNode result = new LinkedListInterview().separateByX(head, 5);
+        // 4 2 3 1 5 7 6 8
+        for (ListNode cur = result; cur != null; cur = cur.next) {
+            System.out.print(cur.val+"-->");
+        }
+    }
+
+    private static ListNode createTestList() {
+        ListNode n1 = new ListNode(4);
+        ListNode n2 = new ListNode(5);
+        ListNode n3 = new ListNode(2);
+        ListNode n4 = new ListNode(7);
+        ListNode n5 = new ListNode(6);
+        ListNode n6 = new ListNode(3);
+        ListNode n7 = new ListNode(8);
+        ListNode n8 = new ListNode(1);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        n6.next = n7;
+        n7.next = n8;
+        n8.next=null;
+
+        return n1;
+    }
+
+    public ListNode separateByX(ListNode head, int x) {
+        ListNode sHead = null; //左半部分
+        ListNode sEnd = null;
+        ListNode bHead = null; //右半部分
+        ListNode bEnd = null;
+
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            if (cur.val < x) {
+                if (sHead == null) {
+                    sHead = cur;
+                } else {
+                    sEnd.next = cur;
+                }
+                sEnd = cur;
+            } else {
+                if (bHead == null) {
+                    bHead = cur;
+                } else {
+                    bEnd.next = cur;
+                }
+                bEnd = cur;
+            }
+        }
+
+        if (sEnd == null) {
+            return bHead;
+        }
+        /*这里有问题,应该让bEnd.next=null;
+        sEnd.next = bHead;
+        return sHead;
+        */
+
+        //改正为
+        sEnd.next = bHead;
+        if(bEnd!=null){
+            bEnd.next=null;  //一定要让链表的最后一个结点指向null
+        }
+        return sHead;
+    }
+}
+
+
+
+41.
+
+
+
+
+
+
 
 
 
