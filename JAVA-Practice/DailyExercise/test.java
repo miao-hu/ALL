35b6502946773d21d86b3905c812bb61294b1819
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-09-07 08:25:38 +0800

    Update test.java

--- JAVA-Practice/DailyExercise/test.java
+++ JAVA-Practice/DailyExercise/test.java
@@ -3796,7 +3796,77 @@ public class MinStackB {
 
 
 
-52.
+52.MyCircularQueue.java
+/*
+循环队列：front rear
+ */
+
+class MyCircularQueue {
+    private int[] array;  //队列采用顺序表定义
+    private int size;   //队列中的元素个数
+    private int front;  //队列的头
+    private int rear;   //队列的尾的下一个位置
+
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        array=new int[k];
+        size=0;  //队列中的元素个数为0
+        front=0;
+        rear=0;
+    }
+
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+        if(size==array.length){   //此时队列已满
+            return false;
+        }
+        array[rear]=value;
+        rear=(rear+1)%array.length;//队列已满，rear==0
+        size++; //队列中元素个数+1
+        return true;
+    }
+
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if(size==0){    //队列中元素个数为0
+            return false;
+        }
+        front=(front+1)%array.length;//队列元素全部删完front==0
+        size--; //队列中元素个数-1
+        return true;
+    }
+
+    /** Get the front item from the queue. */
+    public int Front() {
+        if(size==0){
+            return -1;
+        }
+        return array[front];
+    }
+
+    /** Get the last item from the queue. */
+    public int Rear() {
+        if(size==0){
+            return -1;
+        }
+        int index=(rear-1+array.length)%array.length;
+        return array[index];
+    }
+
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        return size==array.length;
+    }
+}
+
+
+
+53.
 
 
 
