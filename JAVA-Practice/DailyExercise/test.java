448a28aa3e130b18db8713e9ac4256d442a9fe10
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-09-14 21:44:14 +0800

    Update test.java

--- JAVA-Practice/DailyExercise/test.java
+++ JAVA-Practice/DailyExercise/test.java
@@ -4222,23 +4222,39 @@ public class ArrayList extends List{
 
 
 
-58.
+58.class A {
+    static {
+        System.out.println("这是 A 的静态代码块");
+    }
+}
+
+class B extends A {
+    static {
+        System.out.println("这是 B 的静态代码块");
+    }
+}
+
+
+public class ClassLoadOrder extends B {
+    public static void main(String[] args) {
+        System.out.println("开始");
+        new B();
+        new B(); //静态代码始终只被加载一次，所以结果只能打印一次
+        new B();
+        System.out.println("结束");
+    }
+}
+
+/*
+这是 A 的静态代码块
+这是 B 的静态代码块
+开始
+结束
+ */
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+59.
 
 
 
