c775d63a693d3d910b703e5f3b8f6e2db50dfa2f
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-09-19 18:27:40 +0800

    Update test.java

--- JAVA-Practice/DailyExercise/test.java
+++ JAVA-Practice/DailyExercise/test.java
@@ -4455,7 +4455,52 @@ public class List {
 
 
 
-63.
+63.//删除链表中的重复结点
+class Node{
+    int val;
+    Node next;
+}
+public class Soultion {
+    public Node deleteDuplicated(Node head){
+        if(head==null)return null;
+        if(head.next==null)return head;
+        Node prev=new Node();//先创建一个结点
+        prev.next=head;
+        Node first=prev;//保存prev
+        Node p=head;
+        Node q=head.next;//用来遍历链表
+        while(q!=null){
+            if(p.val!=q.val){
+                prev=p;
+                p=q;
+                q=q.next;
+            }else{
+                while(q!=null&&p.val==q.val){
+                    q=q.next;
+                }
+                prev.next=q;
+                p=q;
+                if(q!=null){
+                    q=q.next;
+                }
+            }
+        }
+        return first.next;
+    }
+}
+
+
+
+64.
+
+
+
+
+
+
+
+
+
 
 
 
