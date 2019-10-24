74c02006e0d0e5b2abe6d0449c6267c52b96ec0a
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-10-24 17:40:40 +0800

    Create ExceptionDemo1.java

---
+++ JAVA-Practice/Exception/ExceptionDemo1.java
@@ -0,0 +1,24 @@
+public class ExceptionDemo1 {
+    public static void main(String[] args) {
+        try{
+            throwExceptionMethod(true);
+        }catch(ArrayIndexOutOfBoundsException ex){
+            System.out.println("处理异常");
+        }finally {
+            System.out.println("无论怎样都得来我这一趟");
+        }
+    }
+    /*编译有错误：因为异常类型不匹配
+        抛出异常之前
+        Exception in thread "main" java.lang.NullPointerException
+        无论怎样都得来我这一趟
+     */
+
+    private static void throwExceptionMethod(boolean isThrow) {
+        System.out.println("抛出异常之前");
+        if(isThrow){
+            throw new NullPointerException();
+        }
+        System.out.println("抛出异常之后");
+    }
+}
