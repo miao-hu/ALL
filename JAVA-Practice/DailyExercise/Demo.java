420948c46469c7a7db0e763b33deff28fa1a5365
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-11-29 07:41:17 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -5850,7 +5850,37 @@ public class Main5 {
 
 
 
-101.
+101.import java.util.Scanner;
+/*
+Fibonacci数列是这样定义的：
+F[0] = 0
+F[1] = 1
+for each i ≥ 2: F[i] = F[i-1] + F[i-2]
+因此，Fibonacci数列就形如：0, 1, 1, 2, 3, 5, 8, 13, ...
+在Fibonacci数列中的数我们称为Fibonacci数。给
+你一个N，你想让其变为一个Fibonacci数，每一步你可以把当前数字X变为X-1或者X+1
+现在给你一个数N求最少需要多少步可以变为Fibonacci数。
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=0,b=1,c=1;  //第一个数   第二个数  第三个数
+        while(c<n){   //迭代减少了递归的重复计算
+            a=b;      //找到大于等于n的Fib数就退出循环
+            b=c;      //n一定在b和c之间
+            c=a+b;
+        }
+        int step1=n-b;
+        int step2=c-n;
+        System.out.println((step1<step2)?step1:step2);
+    }
+}
+
+
+
+102.
+
 
 
 
