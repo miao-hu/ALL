937348ac72407fa04bd2be1865f9fed2b2b4faeb
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-11-29 07:41:47 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -5880,7 +5880,25 @@ public class Main1 {
 
 
 102.
+import java.util.*;
+/*
+有一个XxY的网格，一个机器人只能走格点且只能向右或向下走，
+要从左上角走到右下角。请设计一个算法，计算机器人有多少种走法。
 
+给定两个正整数int x,int y，请返回机器人的走法数目。保证x＋y小于等于12。
+ */
+public class Main2 {
+    public int countWays(int x, int y) {
+        if(x==1||y==1){
+            return 1;
+        }
+        return countWays(x-1,y)+countWays(x,y-1);
+    }
+}
+
+
+
+103.
 
 
 
