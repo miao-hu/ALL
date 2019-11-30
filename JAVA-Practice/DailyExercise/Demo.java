e5b80ad99449733a0c72aef0cffb105b5dbf62dd
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-11-30 08:55:38 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -5919,7 +5919,30 @@ public class Solution {
 
 
 
-104.
+104.import java.util.Scanner;
+/*
+输入一个整数，将这个整数以字符串的形式逆序输出
+程序不考虑负数的情况，若数字含有0，则逆序形式也含有0，
+如输入为100，则输出为001
+ */
+public class Main44 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s="";
+        while(n!=0){
+            int t1=n%10;
+            int t2=n/10;
+            s=s+String.valueOf(t1);
+            n=t2;
+        }
+        System.out.println(s);
+    }
+}
+
+
+
+105.
 
 
 
