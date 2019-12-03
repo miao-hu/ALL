4cfd5f81cc4a89db71c114b0b57d43c286fe0180
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-03 16:19:23 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -6143,8 +6143,44 @@ public class Main {
 
 
 
-113.
+113.import java.util.Scanner;
+/*
+给定数字0-9各若干个。你可以以任意顺序排列这些数字，但必须全部使用。
+目标是使得最后得到的数尽可能小（注意0不能做首位）。例如：
 
+给定两个0，两个1，三个5，一个8，我们得到的最小的数就是10015558。
+
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[10];
+            for (int i = 0; i < 10; i++) {
+                a[i] = sc.nextInt();
+            }
+            String s = "";
+            if (a[0] != 0) {
+                for (int i = 1; i < 10; i++) {
+                    if (a[i] != 0) {
+                        s += i + "";
+                        a[i]--;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < 10; i++) {
+                while (a[i] != 0) {
+                    s += i + "";
+                    a[i]--;
+                }
+            }
+            System.out.println(s);
+    }
+}
+
+
+
+114.
 
 
 
