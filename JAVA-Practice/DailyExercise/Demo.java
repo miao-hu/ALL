23b2b17713ee944ea432152995a089c2d141ad05
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-05 10:49:17 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -6501,7 +6501,23 @@ public class Solution {
 
 
 125.
+/*
+现有一个小写英文字母组成的字符串s和一个包含较短小写英文字符串的数组p，
+请设计一个高效算法，对于p中的每一个较短字符串，判断其是否为s的子串。
+ */
+public class Main11 {
+    public boolean[] chkSubStr(String[] p, int n, String s) {
+        boolean[] b=new boolean[n];
+        for(int i=0;i<n;i++){
+            b[i]=s.contains(p[i]);
+        }
+        return b;
+    }
+}
 
+
+
+126.
 
 
 
