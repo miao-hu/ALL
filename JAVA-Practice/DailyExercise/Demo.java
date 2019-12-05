6033f8fab7fadbb4c4e981a54c8da94c3e7ea70c
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-05 10:51:04 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -6642,7 +6642,27 @@ public class Main1{
 
 
 
-129.
+129./*
+一个百万富翁遇到一个陌生人，陌生人找他谈了一个换钱的计划。
+该计划如下：我每天给你10 万元，你第一天给我1 分钱，第二天2 分钱，
+第三天4 分钱……这样交换 30 天后，
+百万富翁交出了多少钱？陌生人交出了多少钱？
+ */
+public class Main22 {
+    public static void main(String[] args) {
+        long furen=0;
+        long qiongren=0;
+        for(int i=1;i<=30;i++){
+            furen+=10;
+            qiongren+=Math.pow(2,i-1);
+        }
+        System.out.println(furen+" "+qiongren);
+    }
+}
+
+
+
+130.
 
 
 
