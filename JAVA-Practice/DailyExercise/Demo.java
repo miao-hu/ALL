51dce289b5ea99d600ba9d0b93bdb882e8e8d75e
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-08 08:37:57 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -6933,7 +6933,33 @@ public class Main {
 
 
 
-137.
+137.import java.util.Scanner;
+/*
+有一只兔子，从出生后第3个月起每个月都生一只兔子，
+小兔子长到第三个月后每个月又生一只兔子，
+假如兔子都不死，问每个月的兔子总数为多少？
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            int month=sc.nextInt();
+            System.out.println(Fib(month));
+        }
+    }
+
+    private static int Fib(int month) {
+        if(month==1||month==2){
+            return 1;
+        }else{
+            return Fib(month-1)+Fib(month-2);
+        }
+    }
+}
+
+
+
+138.
 
 
 
