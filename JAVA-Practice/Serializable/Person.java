03641ff39a2f9c2ea34d4b8dc2cae8b886ea1df7
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-21 08:35:46 +0800

    Create Person.java

---
+++ JAVA-Practice/Serializable/Person.java
@@ -0,0 +1,16 @@
+import java.io.Serializable;
+
+public class Person implements Serializable{
+    String name;
+    int age;
+
+    public Person(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person{ %s : %d }",name,age);
+    }
+}
