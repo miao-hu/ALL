7d170a27eabf05920cc6b40e2f96d83837af4721
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-22 08:26:01 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8130,13 +8130,42 @@ public class TryWithResource {
 
 
 162.
+import java.util.Scanner;
+/*
+题目描述
+        •连续输入字符串，请按长度为8拆分每个字符串后输出到新的字符串数组；
+        •长度不是8整数倍的字符串请在后面补数字0，空字符串不处理。
+输入描述:
+连续输入字符串(输入2次,每个字符串长度小于100)
+
+输出描述:
+输出到长度为8的新字符串数组
+         abc
+         123456789
+
+         abc00000
+         12345678
+         90000000
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            String s=sc.nextLine();
+            if(s.length()%8!=0){
+                s=s+"00000000";
+            }
+            while(s.length()>=8){
+                System.out.println(s.substring(0,8));
+                s=s.substring(8);
+            }
+        }
+    }
+}
 
 
 
-
-
-
-
+163.
 
 
 
