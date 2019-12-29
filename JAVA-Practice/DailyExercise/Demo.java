cf7cd6551394f97e305906384fd10511cfc39022
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-29 08:57:53 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8226,7 +8226,50 @@ public class Solution1 {
 
 
 
-165.
+165./*
+请实现一个函数，用来判断一颗二叉树是不是对称的。
+注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的
+ */
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+
+public class Solution3 {
+    boolean isSymmetrical(TreeNode pRoot) {
+        if(pRoot==null){
+            return true;
+        }
+        return panduan(pRoot.left,pRoot.right);
+    }
+
+    private boolean panduan(TreeNode left, TreeNode right) {
+        if(left==null){
+            return right==null;
+        }
+        if(right==null){
+            return false;
+        }
+        if(left.val!=right.val){
+            return false;
+        }
+        return panduan(left.left,right.right)&&panduan(left.right,right.left);
+    }
+}
+
+
+
+166.
+
+
+
 
 
 
