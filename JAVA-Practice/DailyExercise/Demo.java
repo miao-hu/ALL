58bf8d2b800ddf514a5d3bdf4fb0b8798de4d8b5
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-29 08:57:07 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8165,7 +8165,45 @@ public class Main1 {
 
 
 
-163.
+163.import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/*
+    输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+    例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
+
+    正整数
+ */
+public class Solution {
+    public String PrintMinNumber(int [] numbers) {
+        if(numbers.length==0){
+            return "";
+        }
+        String s="";
+        List<Integer> list=new ArrayList<>();
+        for(int e:numbers){
+            list.add(e);
+        }
+        Collections.sort(list,new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                String s1=o1+""+o2;
+                String s2=o2+""+o1;
+                return s1.compareTo(s2);
+            }
+        });
+        for(int e:list){
+            s+=e;
+        }
+        return s;
+    }
+}
+
+
+
+164.
 
 
 
