6082c771f5e99e0c97b953354ecbab2a379d3620
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-12-30 17:14:43 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8319,7 +8319,44 @@ public class Solution5 {
 
 
 
-168.
+168.import java.util.Map;
+import java.util.LinkedHashMap;
+/*
+请实现一个函数用来找出字符流中第一个只出现一次的字符。
+例如，当从字符流中只读出前两个字符"go"时，第一个只出现一次的字符是"g"。
+当从该字符流中读出前六个字符“google"时，第一个只出现一次的字符是"l"。
+输出描述:
+如果当前字符流没有存在出现一次的字符，返回#字符。
+ */
+public class Solution6 {
+    public Map<Character,Integer> map=new LinkedHashMap<>();
+    //Insert one char from stringstream
+    public void Insert(char ch)
+    {
+        if(map.containsKey(ch)){
+            map.put(ch,map.get(ch)+1);
+        }else{
+            map.put(ch,1);
+        }
+    }
+    //return the first appearence once char in current stringstream
+    public char FirstAppearingOnce()
+    {
+        char ret='#';
+        for(Map.Entry<Character,Integer> e:map.entrySet()){
+            int count=e.getValue();
+            if(count==1){
+                return e.getKey();
+            }
+        }
+        return ret;
+    }
+}
+
+
+
+169.
+
 
 
 
