b4e972e9395d9267ba1d8e17264a8ccdf721a112
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-01-02 18:03:12 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8462,7 +8462,32 @@ public class Solution10 {
 
 
 
-173.
+173.import java.util.Scanner;
+/*
+将一个英文语句以单词为单位逆序排放。例如“I am a boy”，逆序排放后为“boy a am I”
+所有单词之间用一个空格隔开，语句中除了英文字母外，不再包含其他字符
+ */
+public class Solution11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            String s=sc.nextLine();
+            String[] a=s.split(" ");
+            String[] b=new String[a.length];
+            int k=0;
+            for(int i=a.length-1;i>=0;i--){
+                b[k++]=a[i];
+            }
+            for(String e:b){
+                System.out.print(e+" ");
+            }
+        }
+    }
+}
+
+
+
+174.
 
 
 
