e34c867fd183b0539b84c4588d5a90a522a74ab0
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-01-02 18:03:41 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8487,7 +8487,49 @@ public class Solution11 {
 
 
 
-174.
+174.import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+/*
+数据表记录包含表索引和数值（int范围的整数），请对表索引相同的记录进行合并，
+即将相同索引的数值进行求和运算，输出按照key值升序进行输出
+
+输入
+        4
+        0 1
+        0 2
+        1 2
+        3 4
+输出
+        0 3
+        1 2
+        3 4
+ */
+public class Solution12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while (sc.hasNext()){
+            int n=sc.nextInt();
+            Map<Integer,Integer> map=new TreeMap<>();
+            for(int i=0;i<n;i++){
+                int key=sc.nextInt();
+                int value=sc.nextInt();
+                if(map.containsKey(key)){
+                    map.put(key,map.get(key)+value);
+                }else{
+                    map.put(key,value);
+                }
+            }
+            for(Map.Entry<Integer,Integer> e:map.entrySet()){
+                System.out.println(e.getKey()+" "+e.getValue());
+            }
+        }
+    }
+}
+
+
+
+175.
 
 
 
