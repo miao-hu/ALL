67ef33d73ef4b1abd386e70549c7ac67ae6e03d7
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-01-15 18:09:19 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8926,7 +8926,52 @@ public class Solution4 {
 
 
 
-190.
+190./*
+    给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ */
+public class Solution22 {
+    public void rotate(int[] nums, int k) {
+        int t,pre;
+        for(int i=0;i<k;i++){
+            pre=nums[nums.length-1];
+            for(int j=0;j<nums.length;j++){
+                t=nums[j];
+                nums[j]=pre;
+                pre=t;
+            }
+        }
+    }
+}
+
+
+
+191./*
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+你可以假设数组中无重复元素。
+ */
+public class Solution44 {
+    public int searchInsert(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+}
+
+
+
+192.
 
 
 
