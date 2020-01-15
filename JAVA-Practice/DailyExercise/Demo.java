2421a7fc88214b511a2ed01f9b1df028cdfb03d9
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-01-15 10:07:21 +0000

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8833,7 +8833,41 @@ public class Solution23 {
 
 
 
-186.
+186./*
+    实现函数 ToLowerCase()，该函数接收一个字符串参数 str，
+    并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+ */
+public class Solution1 {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
+
+
+187./*
+    给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ */
+public class Solution2 {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+
+
+188.
 
 
 
