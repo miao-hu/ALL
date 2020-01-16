8aafecc07ec7114e6fddc48f344fc28987747739
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-01-16 12:39:39 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -8971,7 +8971,63 @@ public class Solution44 {
 
 
 
-192.
+192./*
+判断一个整数是否是回文数。
+回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ */
+public class Solution5 {
+    public boolean isPalindrome(int x) {
+        String s=String.valueOf(x);
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        if(s.equals(sb.toString())){
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+193.import java.util.HashMap;
+import java.util.Map;
+/*
+给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，
+判断第一个字符串ransom能不能由第二个字符串magazines里面的字符构成。
+如果可以构成，返回 true ；否则返回 false。
+
+ */
+public class Solution6 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer> map1=new HashMap<>();
+        Map<Character,Integer> map2=new HashMap<>();
+        char[] a=ransomNote.toCharArray();
+        char[] b=magazine.toCharArray();
+        for(char c:a){
+            int count=map1.getOrDefault(c,0);
+            map1.put(c,count+1);
+        }
+        for(char c:b){
+            int count=map2.getOrDefault(c,0);
+            map2.put(c,count+1);
+        }
+        for(Map.Entry<Character,Integer> e:map1.entrySet()){
+            char c=e.getKey();
+            int count=e.getValue();
+            if(!map2.containsKey(c)){
+                return false;
+            }
+            if(map2.get(c)<count){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+194.
 
 
 
