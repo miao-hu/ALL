e5240b6bcfe00b1178937dbc2a960c2787997284
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-01-19 12:14:30 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -9265,7 +9265,50 @@ public class Solution15 {
 }
 
 
-204.
+204./*
+给定一个非空数组，返回此数组中第三大的数。(注意：第三大的数唯一存在)
+如果不存在，则返回数组中最大的数。要求算法时间复杂度必须是O(n)。
+ */
+import java.util.*;
+
+public class Solution16 {
+    public  int thirdMax(int[] nums) {
+        Set<Integer> set=new TreeSet<>();
+        for(int e:nums){
+            set.add(e);
+            if(set.size()>3){
+                set.remove(((TreeSet<Integer>) set).first());
+            }
+        }
+        if(set.size()<3){
+            return ((TreeSet<Integer>) set).last();
+        }
+        return ((TreeSet<Integer>) set).first();
+    }
+}
+
+
+205./*
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ */
+public class Solution17 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr=new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    arr[0]=i;
+                    arr[1]=j;
+                }
+            }
+        }
+        return arr;
+    }
+}
+
+
+206.
 
 
 
