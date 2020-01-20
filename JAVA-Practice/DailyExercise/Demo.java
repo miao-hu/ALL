0c6a8173790669e1aad25718dd4f83b9b64cb602
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-01-20 11:09:33 +0800

    Update Demo.java

--- JAVA-Practice/DailyExercise/Demo.java
+++ JAVA-Practice/DailyExercise/Demo.java
@@ -9515,7 +9515,69 @@ public class Solution23 {
 }
 
 
-212.
+212./*
+根据逆波兰表示法，求表达式的值。
+有效的运算符包括 +, -, *, / 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+
+输入: ["4", "13", "5", "/", "+"]
+输出: 6
+解释: (4 + (13 / 5)) = 6
+ */
+import java.util.Stack;
+
+public class Solution24 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack=new Stack<>();
+        for(String s:tokens){
+            if(s.equals("+")){
+                stack.push(stack.pop()+stack.pop());
+            }else if(s.equals("-")){
+                stack.push(-stack.pop()+stack.pop());
+            }else if(s.equals("*")){
+                stack.push(stack.pop()*stack.pop());
+            }else if(s.equals("/")){
+                int chushu=stack.pop();
+                stack.push(stack.pop()/chushu);
+            }else{
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
+
+
+213.import java.util.LinkedList;
+import java.util.List;
+
+public class Solution25 {
+        private static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        private int m, n;
+
+        public int longestIncreasingPath(int[][] matrix) {
+            if (matrix.length == 0) return 0;
+            m = matrix.length;
+            n = matrix[0].length;
+            int ans = 0;
+            for (int i = 0; i < m; ++i)
+                for (int j = 0; j < n; ++j)
+                    ans = Math.max(ans, dfs(matrix, i, j));
+            return ans;
+        }
+
+        private int dfs(int[][] matrix, int i, int j) {
+            int ans = 0;
+            for (int[] d : dirs) {
+                int x = i + d[0], y = j + d[1];
+                if (0 <= x && x < m && 0 <= y && y < n && matrix[x][y] > matrix[i][j])
+                    ans = Math.max(ans, dfs(matrix, x, y));
+            }
+            return ++ans;
+        }
+    }
+
+
+214
 
 
 
