f84587da3e817e9447f790273666880e6322d43c
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-05-26 07:44:46 +0800

    Create 定位link_text.py

---
+++ Selenium/SeleniumStudy/Phase1/定位link_text.py
@@ -0,0 +1,12 @@
+# coding : utf-8
+from selenium import webdriver    # 导入 webdriver ,相当于加载了 webdriver 里面所有的方法
+import time                       # 导入 time 包
+
+driver = webdriver.Chrome()       # 创建 chrome 对象
+driver.get("https://www.baidu.com/")
+time.sleep(2)                     # 网页打开后等待停止2秒
+
+driver.find_element_by_link_text("hao123").click()     # 查找页面内容为hao123的，然后并点击
+time.sleep(5)
+
+driver.quit()                     # 关闭 chrome 对象
