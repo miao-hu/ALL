aea52e4ba8f4d41fd4bc4917f34b801c2920b0b8
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-05-25 09:22:03 +0800

    Create 关闭窗口.py

---
+++ Selenium/SeleniumStudy/Phase1/关闭窗口.py
@@ -0,0 +1,12 @@
+# coding : utf-8
+from selenium import webdriver    # 导入 webdriver ,相当于加载了 webdriver 里面所有的方法
+import time                       # 导入 time 包
+
+driver = webdriver.Chrome()       # 创建 chrome 对象
+driver.get("https://www.csdn.net/")
+time.sleep(5)                     # 网页打开后等待停止5秒
+driver.refresh()                  # 刷新页面
+time.sleep(5)                     # 目的是为了查看效果
+
+driver.quit()                     # 关闭 chrome 对象
+# driver.close()
