7d04ebf3ce52a36d512411899321d0ead1c8d8c4
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-05-28 07:52:19 +0800

    Create 截屏.py

---
+++ Selenium/SeleniumStudy/Phase1/截屏.py
@@ -0,0 +1,15 @@
+# coding : utf-8
+from selenium import webdriver    # 导入 webdriver ,相当于加载了 webdriver 里面所有的方法
+import time                       # 导入 time 包
+
+driver = webdriver.Chrome()       # 创建 chrome 对象
+driver.get("https://www.csdn.net/")
+
+time.sleep(3)                     # 网页打开后等待停止3秒
+driver.set_window_size(800,600)   # 固定大小显示
+
+driver.get_screenshot_as_file(".\\jieping.png")
+
+driver.refresh()                  # 刷新页面
+time.sleep(5)                     # 目的是为了查看效果
+driver.quit()                     # 关闭 chrome 对象
