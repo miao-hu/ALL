c19aaafa4fc5a887d5d42c4fab1b1d1506c91cdd
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-05-28 07:52:04 +0800

    Create 键盘输入.py

---
+++ Selenium/SeleniumStudy/Phase1/键盘输入.py
@@ -0,0 +1,13 @@
+# coding : utf-8
+from selenium import webdriver    # 导入 webdriver ,相当于加载了 webdriver 里面所有的方法
+import time                       # 导入 time 包
+
+driver = webdriver.Chrome()       # 创建 chrome 对象
+driver.get("https://www.baidu.com/")
+time.sleep(2)                     # 网页打开后等待停止2秒
+
+shuru = driver.find_element_by_id("kw")
+shuru.send_keys("你好世界")
+time.sleep(3)
+
+driver.quit()                     # 关闭 chrome 对象
