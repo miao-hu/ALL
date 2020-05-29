7a87cf5ecd8ecd626f791dbdd2499e7db5b68d97
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-05-29 08:49:01 +0800

    Create 鼠标拖拽.py

---
+++ Selenium/SeleniumStudy/Phase1/鼠标拖拽.py
@@ -0,0 +1,15 @@
+# coding : utf-8
+from selenium import webdriver    # 导入 webdriver ,相当于加载了 webdriver 里面所有的方法
+import time                       # 导入 time 包
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()       # 创建 chrome 对象
+driver.get("https://www.baidu.com/")
+time.sleep(2)                     # 网页打开后等待停止2秒
+
+a = driver.find_element_by_link_text("hao123")     # 查找页面内容为hao123的句柄
+b = driver.find_element_by_link_text("地图")
+ActionChains(driver).drag_and_drop(a,b).perform()
+time.sleep(3)
+
+driver.quit()                     # 关闭 chrome 对象
